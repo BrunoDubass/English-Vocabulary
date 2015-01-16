b01ff496af451cc26db0d35bdcf87a9ba037f278
Bruno Domínguez <brunodominguezbascones@yahoo.es>
2015-01-16 10:24:36 +0100

    Initial Commit

---
+++ English Vocabulary/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  English Vocabulary
+//
+//  Created by Bruno Domínguez on 16/01/15.
+//  Copyright (c) 2015 brunodominguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ English VocabularyTests/English_VocabularyTests.m
@@ -0,0 +1,40 @@
+//
+//  English_VocabularyTests.m
+//  English VocabularyTests
+//
+//  Created by Bruno Domínguez on 16/01/15.
+//  Copyright (c) 2015 brunodominguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface English_VocabularyTests : XCTestCase
+
+@end
+
+@implementation English_VocabularyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
